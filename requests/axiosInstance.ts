6bf8cb92cd5b508783826eee82f44374d94cefb6
woobsit <woobsit@gmail.com>
2025-07-28 18:16:48 +0100

    WIP: changed endpoint

--- requests/axiosInstance.ts
+++ requests/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { attachAuthToken, refreshAuthToken, clearTokens } from '../utils/apiHelpers';
 
 const api = axios.create({
-  baseURL: 'http://192.168.0.116:5000/api/v1',
+  baseURL: 'http://192.168.0.120:5000/api/v1',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
